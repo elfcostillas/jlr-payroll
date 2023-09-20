b96849bc768f2ea7b8b05f7a17f96a045c2a9fa3
elmer <elfcostillas@gmail.com>
2023-09-20 09:22:49 +0800

    leave updates

--- resources/views/app/reports/leave-reports/leave-employee-excel.blade.php
+++ resources/views/app/reports/leave-reports/leave-employee-excel.blade.php
@@ -14,18 +14,19 @@
     }
 
 ?>
-<table>
+<table border=1 style="border-collapse:collapse;" >
     <tr>
-        <td>Name</td>
-        <td>Date</td>
-        <td>Type</td>
-        <td>Reason</td>
-        <td>With Pay</td>
-        <td>Without Pay</td>
-        <td>Under Time</td>
-        <td>SVL</td>
-        <td>VL Balance</td>
-        <td>SL Balance  </td>
+        <td style="border:1px solid black;"  >Name</td>
+        <td style="border:1px solid black;">Date</td>
+        <td style="border:1px solid black;">Type</td>
+        <td style="border:1px solid black;">Reason</td>
+        <td style="border:1px solid black;">With Pay</td>
+        <td style="border:1px solid black;">Without Pay</td>
+        <td style="border:1px solid black;">Birthday Leave</td>
+        <td style="border:1px solid black;">Under Time</td>
+        <td style="border:1px solid black;">SVL</td>
+        <td style="border:1px solid black;">VL Balance</td>
+        <td style="border:1px solid black;">SL Balance  </td>
     </tr>
     <tr>
         <td></td>
@@ -33,23 +34,24 @@
     </tr>
     @foreach($data as $emp)
         <tr>
-            <td> {{ $emp->employee_name }} </td>
+            <td style="border:1px solid black;"> {{ $emp->employee_name }} </td>
         </tr>
        
         @foreach($emp->leaves as $leave)
           
             <tr>
-                <td></td>
-                <td>{{ $leave->mask_leave_date }}</td>
-                <td>{{ $leave->leave_type }}</td>
-                <td>{{ $leave->remarks }}</td>
+                <td style="border:1px solid black;"></td>
+                <td style="border:1px solid black;">{{ $leave->mask_leave_date }}</td>
+                <td style="border:1px solid black;">{{ $leave->leave_type }}</td>
+                <td style="border:1px solid black;">{{ $leave->remarks }}</td>
                 
-                <td>{{ nformat(($leave->leave_type!='UT' && $leave->leave_type!='SVL') ? $leave->with_pay : 0 )  }}</td>
-                <td>{{ nformat(($leave->leave_type!='UT' && $leave->leave_type!='SVL') ? $leave->without_pay : 0 )  }}</td>
-                <td>{{ nformat(($leave->leave_type=='UT' && $leave->leave_type!='SVL') ? ($leave->without_pay + $leave->with_pay) : 0 )  }}</td>
-                <td>{{ nformat(($leave->leave_type=='SVL') ? ($leave->without_pay + $leave->with_pay) : 0 )  }}</td>
-                <td>{{ $leave->bal[0]->vacation_leave - $leave->bal[0]->VL_PAY }}</td>
-                <td>{{ $leave->bal[0]->sick_leave - $leave->bal[0]->SL_PAY }}</td>
+                <td style="border:1px solid black;">{{ nformat(($leave->leave_type!='UT' && $leave->leave_type!='SVL' && $leave->leave_type!='BL') ? $leave->with_pay : 0 )  }}</td>
+                <td style="border:1px solid black;">{{ nformat(($leave->leave_type!='UT' && $leave->leave_type!='SVL' && $leave->leave_type!='BL') ? $leave->without_pay : 0 )  }}</td>
+                <td style="border:1px solid black;">{{ nformat(($leave->leave_type=='BL') ? $leave->without_pay + $leave->with_pay : 0 )  }}</td>
+                <td style="border:1px solid black;">{{ nformat(($leave->leave_type=='UT' && $leave->leave_type!='SVL' && $leave->leave_type!='BL') ? ($leave->without_pay + $leave->with_pay) : 0 )  }}</td>
+                <td style="border:1px solid black;">{{ nformat(($leave->leave_type=='SVL') ? ($leave->without_pay + $leave->with_pay) : 0 )  }}</td>
+                <td style="border:1px solid black;">{{ $leave->bal[0]->vacation_leave - $leave->bal[0]->VL_PAY }}</td>
+                <td style="border:1px solid black;">{{ $leave->bal[0]->sick_leave - $leave->bal[0]->SL_PAY }}</td>
             </tr>
         @endforeach
     @endforeach
