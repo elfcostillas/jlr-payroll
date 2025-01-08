d7de21792748665a750f8c490810d645dfe16b67
elmer <elfcostillas@gmail.com>
2025-01-08 22:59:47 +0800

    update db

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-        /*
+       
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -88,7 +88,8 @@ return [
                 PDO::ATTR_EMULATE_PREPARES => true,
             ]) : [],
         ],
-         */
+         /*
+        
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -110,7 +111,7 @@ return [
                 PDO::ATTR_EMULATE_PREPARES => true,
             ]) : [],
         ],
-       
+        */
 		/*
 			'att' => [
 				'driver' => 'mysql',
