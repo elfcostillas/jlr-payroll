7b61cbb17ad85c7b5194307e195498255d4f0c65
root <root@LAPTOP-69PEDAVE.>
2025-02-05 16:43:23 +0800

    bl and tardy

--- app/Mappers/EmployeeFileMapper/Repository/Employee.php
+++ app/Mappers/EmployeeFileMapper/Repository/Employee.php
@@ -158,7 +158,7 @@ class Employee
 
         $this->setDaysWorked();
 
-        $this->payreg['late_eq_amount'] = round($this->rates['minute_rate'] * $this->payreg['late'],2);
+        $this->payreg['late_eq_amount'] = round($this->rates['hourly_rate'] * $this->payreg['late_eq'],2);
         $this->payreg['under_time_amount'] = round($this->rates['hourly_rate'] * $this->payreg['under_time'],2);
         $this->payreg['absences_amount'] = round($this->rates['hourly_rate'] * $this->payreg['absences'],2);
 
