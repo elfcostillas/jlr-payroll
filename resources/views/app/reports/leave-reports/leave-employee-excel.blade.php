a52b4cdbc5d5272148640c20827dbf8f1b58be6c
root <root@LAPTOP-69PEDAVE.>
2025-07-08 11:01:22 +0800

    leave reports update

--- resources/views/app/reports/leave-reports/leave-employee-excel.blade.php
+++ resources/views/app/reports/leave-reports/leave-employee-excel.blade.php
@@ -16,17 +16,33 @@
 ?>
 <table border=1 style="border-collapse:collapse;" >
     <tr>
-        <td style="border:1px solid black;">Name</td>
-        <td style="border:1px solid black;">Date</td>
-        <td style="border:1px solid black;">Type</td>
-        <td style="border:1px solid black;">Reason</td>
+        <td rowspan="2" style="border:1px solid black;">Name</td>
+        <td rowspan="2" style="border:1px solid black;">Date</td>
+        <!-- <td rowspan="2" style="border:1px solid black;">Type</td> -->
+        <td rowspan="2" style="border:1px solid black;">Reason</td>
+        <td colspan="2" style="border:1px solid black;">VL</td>
+        <td colspan="2" style="border:1px solid black;">SL</td>
+        
+        <td rowspan="2" style="border:1px solid black;">Birthday Leave</td>
+        <td rowspan="2" style="border:1px solid black;">Under Time</td>
+        <td rowspan="2" style="border:1px solid black;">SVL</td>
+        <td rowspan="2" style="border:1px solid black;">Bereavement</td>
+        <td rowspan="2" style="border:1px solid black;">Maternity/Paternity Leave</td>
+        <!-- <td style="border:1px solid black;">VL Balance</td>
+        <td style="border:1px solid black;">SL Balance  </td> -->
+    </tr>
+    <tr>
+       
         <td style="border:1px solid black;">With Pay</td>
         <td style="border:1px solid black;">Without Pay</td>
-        <td style="border:1px solid black;">Birthday Leave</td>
-        <td style="border:1px solid black;">Under Time</td>
-        <td style="border:1px solid black;">SVL</td>
-        <td style="border:1px solid black;">VL Balance</td>
-        <td style="border:1px solid black;">SL Balance  </td>
+
+        <td style="border:1px solid black;">With Pay</td>
+        <td style="border:1px solid black;">Without Pay</td>
+
+       
+
+        <!-- <td style="border:1px solid black;">VL Balance</td>
+        <td style="border:1px solid black;">SL Balance  </td> -->
     </tr>
     <!-- <tr>
         <td></td>
@@ -43,16 +59,18 @@
             <tr>
                 <td style="border:1px solid black;"></td>
                 <td style="border:1px solid black;">{{ $leave->mask_leave_date }}</td>
-                <td style="border:1px solid black;">{{ $leave->leave_type }}</td>
+                <!-- <td style="border:1px solid black;">{{ $leave->leave_type }}</td> -->
                 <td style="border:1px solid black;">{{ $leave->remarks }}</td>
                 
-                <td style="border:1px solid black;">{{ nformat(($leave->leave_type!='UT' && $leave->leave_type!='SVL' && $leave->leave_type!='BL') ? $leave->with_pay : 0 )  }}</td>
-                <td style="border:1px solid black;">{{ nformat(($leave->leave_type!='UT' && $leave->leave_type!='SVL' && $leave->leave_type!='BL') ? $leave->without_pay : 0 )  }}</td>
+                <td style="border:1px solid black;"> {{ nformat(($leave->leave_type=='VL') ? $leave->with_pay : 0  ) }} </td>  <!-- VL with pay  -->
+                <td style="border:1px solid black;"> {{ nformat(($leave->leave_type=='VL') ? $leave->without_pay : 0  ) }}</td>  <!-- VL with out pay  -->
+                <td style="border:1px solid black;"> {{ nformat(($leave->leave_type=='SL') ? $leave->with_pay : 0  ) }} </td>  <!-- VL with pay  -->
+                <td style="border:1px solid black;"> {{ nformat(($leave->leave_type=='SL') ? $leave->with_pay : 0  ) }} </td>  <!-- VL with pay  -->
                 <td style="border:1px solid black;">{{ nformat(($leave->leave_type=='BL') ? $leave->without_pay + $leave->with_pay : 0 )  }}</td>
                 <td style="border:1px solid black;">{{ nformat(($leave->leave_type=='UT' && $leave->leave_type!='SVL' && $leave->leave_type!='BL') ? ($leave->without_pay + $leave->with_pay) : 0 )  }}</td>
                 <td style="border:1px solid black;">{{ nformat(($leave->leave_type=='SVL') ? ($leave->without_pay + $leave->with_pay) : 0 )  }}</td>
-                <td style="border:1px solid black;">{{ $leave->bal[0]->vacation_leave - $leave->bal[0]->VL_PAY }}</td>
-                <td style="border:1px solid black;">{{ $leave->bal[0]->sick_leave - $leave->bal[0]->SL_PAY }}</td>
+                <td style="border:1px solid black;">{{ nformat(($leave->leave_type=='BRV') ? ($leave->without_pay + $leave->with_pay) : 0 )  }}</td>
+                <td style="border:1px solid black;">{{ nformat(($leave->leave_type=='MP') ? ($leave->without_pay + $leave->with_pay) : 0 )  }}</td>
             </tr>
         @endforeach
     @endforeach
