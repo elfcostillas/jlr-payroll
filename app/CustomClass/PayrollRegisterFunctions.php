e53956b3e1af4310b7449ad03e3462e102212d4f
root <root@LAPTOP-69PEDAVE.>
2026-08-20 11:39:00 +0800

    added ot again

--- app/CustomClass/PayrollRegisterFunctions.php
+++ app/CustomClass/PayrollRegisterFunctions.php
@@ -1069,14 +1069,14 @@ class PayrollRegisterFunctions
         $result = $this->mainQuery()->get();
         // lala request 07/24/2026 dont show < 50
         $data = [
-            /*
-            remove for testing 08/05/2026
+            
+            //remove for testing 08/05/2026
             'Less than 10 Hrs' => 0,
             '10 Hrs' => 0,
             '20 Hrs' => 0,
             '30 Hrs' => 0,
             '40 Hrs' => 0,
-            */
+            
             '50 Hrs' => 0,
             '60 Hrs' => 0,
             '70 Hrs' => 0,
@@ -1091,6 +1091,7 @@ class PayrollRegisterFunctions
 
               /*
             remove for testing 08/05/2026
+               */
             if($employee->reg_ot > 1 && $employee->reg_ot < 10)
             {
                 $data['Less than 10 Hrs'] += 1;
@@ -1116,7 +1117,7 @@ class PayrollRegisterFunctions
             {
                 $data['40 Hrs'] += 1;
             }
-            */
+         
             // if($employee->reg_ot >= 50)
             if($employee->reg_ot >= 50 && $employee->reg_ot < 60)
             {
